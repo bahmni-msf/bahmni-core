996b127c3ced90e0464b465adabf343722e7bd96
binduak <tbindu@thoughtworks.com>
2021-06-03 10:11:06 +0530

    MOBN-1653 |Bindu | Add regex validation check for formFieldPath for form2 forms

--- bahmnicore-api/src/test/java/org/bahmni/module/bahmnicore/util/MiscUtilsTest.java
+++ bahmnicore-api/src/test/java/org/bahmni/module/bahmnicore/util/MiscUtilsTest.java
@@ -11,6 +11,8 @@ import java.util.Collection;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -43,6 +45,53 @@ public class MiscUtilsTest {
 
         assertThat(observation1.getUuid(), is("123"));
         verify(observation2, times(1)).setUuid(anyString());
+    }
+
+    @Test
+    public void shouldReturnTrueForValidFormFieldPath() {
+        boolean isValidFormFieldPath = MiscUtils.isValidFormFieldPath("Initial Medical Examination.2/8-0/139-0/140-34");
+        assertTrue(isValidFormFieldPath);
+    }
+
+    @Test
+    public void shouldReturnFalseForFormFieldPathWithHyphenFollowedByNoNumber() {
+        boolean isValidFormFieldPath = MiscUtils.isValidFormFieldPath("Initial Medical Examination.2/8-0/139-/140-34");
+        assertFalse(isValidFormFieldPath);
+    }
+
+    @Test
+    public void shouldReturnTrueForFormFieldPathWithUnderscoresAndDigitsInTheFormName() {
+        boolean isValidFormFieldPath = MiscUtils.isValidFormFieldPath("Initial_Medical_Examination_1.2/8-0/139-1/140-34");
+        assertTrue(isValidFormFieldPath);
+    }
+
+    @Test
+    public void shouldReturnTrueForFormFieldPathWithNoSpacesInTheFormName() {
+        boolean isValidFormFieldPath = MiscUtils.isValidFormFieldPath("InitialMedicalExamination1.2/8-0/139-1/140-34");
+        assertTrue(isValidFormFieldPath);
+    }
+
+    @Test
+    public void shouldReturnFalseForFormFieldPathWithSlashAtTheEnd() {
+        boolean isValidFormFieldPath = MiscUtils.isValidFormFieldPath("InitialMedicalExamination1.2/8-0/139-1/140-34/");
+        assertFalse(isValidFormFieldPath);
+    }
+
+    @Test
+    public void shouldReturnFalseForFormFieldPathWithSlashAtTheEndFollowedByANumber() {
+        boolean isValidFormFieldPath = MiscUtils.isValidFormFieldPath("InitialMedicalExamination1.2/8-0/139-1/140-34/2");
+        assertFalse(isValidFormFieldPath);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowExceptionIfTheFormFieldPathIsNotValid() {
+        BahmniObservation observation1 = new BahmniObservation();
+        observation1.setUuid("123");
+        observation1.setFormFieldPath("InitialMedicalExamination1.2/8-0/139-/140-34");
+        BahmniObservation observation2 = mock(BahmniObservation.class);
+        Collection<BahmniObservation> bahmniObservations = Arrays.asList(observation1, observation2);
+
+        MiscUtils.validateFormFieldPath(bahmniObservations);
     }
 
 }
