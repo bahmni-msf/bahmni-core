8319c4c283a08f4e69846e0cf944cc9028fc7aa4
supriya <supriya.muppiri@thoughtworks.com>
2021-11-17 18:15:09 +0530

    Supriya | MOBN-1802 | Add missing argument to csvPatientService test

--- admin/src/test/java/org/bahmni/module/admin/csv/service/CSVPatientServiceTest.java
+++ admin/src/test/java/org/bahmni/module/admin/csv/service/CSVPatientServiceTest.java
@@ -162,7 +162,7 @@ public class CSVPatientServiceTest {
         when(mockPatientService.getPatientIdentifierTypeByName(extraIdentifierName2)).thenReturn(patientIdentifierType2);
 
         ArgumentCaptor<Patient> patientArgumentCaptor = ArgumentCaptor.forClass(Patient.class);
-        CSVPatientService csvPatientService = new CSVPatientService(mockPatientService, mockPersonService, conceptService, mockAdminService, csvAddressService);
+        CSVPatientService csvPatientService = new CSVPatientService(mockPatientService, mockPersonService, conceptService, mockAdminService, csvAddressService,registrationPageService);
 
         csvPatientService.save(patientRow);
 
@@ -182,7 +182,7 @@ public class CSVPatientServiceTest {
         }};
         patientRow.identifiers = identifiers;
 
-        CSVPatientService csvPatientService = new CSVPatientService(mockPatientService, mockPersonService, conceptService, mockAdminService, csvAddressService);
+        CSVPatientService csvPatientService = new CSVPatientService(mockPatientService, mockPersonService, conceptService, mockAdminService, csvAddressService,registrationPageService);
 
         exception.expect(RuntimeException.class);
         exception.expectMessage("Invalid identifier name: Invalid ID");
